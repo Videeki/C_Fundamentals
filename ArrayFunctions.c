e6a4efece2af5721164c3a7398b8fb36da9d5c86
Videeki <videeki@gmail.com>
2023-07-19 21:55:43 +0200

    Add IndexArray function

--- ArrayFunctions.c
+++ ArrayFunctions.c
@@ -3,13 +3,22 @@
 #include <string.h>
 
 int Sort1DArray(char *input[]);
+int IndexArray(int array[], int index);
 
 int main(int argc, char *argv[])
 {
     char *arr[10] = {"alma", "banan", "citrom", "narancs", "kalacs"};
     printf("Size of the array: %d\n", sizeof(arr)/sizeof(*arr));
     Sort1DArray(arr);
+    
+    int array[] = {0,1,2,3,4,5,6,7,8,9};
+    printf("The array size is: %d\n", IndexArray(array, 3));
     return 0;
+}
+
+int IndexArray(int array[], int index)
+{
+    return array[index];
 }
 
 int Sort1DArray(char *input[])
