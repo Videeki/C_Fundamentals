00ab51c74ce427a400033cb77e8bac6d8ff77be6
Videeki <videeki@gmail.com>
2023-08-06 11:45:09 +0200

    Finish the GPIOHandler file. Finally it works!

--- GPIOHandler.c
+++ GPIOHandler.c
@@ -1,27 +1,93 @@
 // Compiling: gcc GPIOHandler.c -o GPIOHandler -l wiringPi
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
 
-#include <wiringPi.h>
+#define ON 1
+#define OFF 0
 
-void blink (const int led)
+int initPIN(int pinNr);
+int setupPIN(int pinNr, char *mode);
+int writePIN(int pinNr, int value);
+int readPIN(int pinNr);
+int deinitPIN(int pinNr);
+
+int main(int argc, char* argv[])
 {
-    digitalWrite(led, HIGH);
-    delay(30);
-    digitalWrite(led, LOW);
-    delay(30);
+    //int LED = atoi(argv[1]);
+    int LED = 23;
+    char mode[] = "out";
+
+    initPIN(LED);
+    puts("Init DONE!");
+    setupPIN(LED, mode);
+    puts("Setup DONE!");
+    puts("Let's get ready to RUMBLE!!!");
+
+    for(int i = 0; i < 10; i++)
+    {
+        writePIN(LED, ON);
+        puts("ON");
+        sleep(1);
+        writePIN(LED, OFF);
+        puts("OFF");
+        sleep(1);
+    }
+    puts("GAME OVER");
+    deinitPIN(LED);
+    puts("Deinit DONE!");
+    puts("Good Bye Sucker!");
 }
 
-int main(void)
+int initPIN(int pinNr)
 {
-    int led = 23;
-    wiringPiSetupGpio();
+    char initPin[33];
+    sprintf(initPin, "echo %d > /sys/class/gpio/export", pinNr);
+    system(initPin);
+    sleep(1);
+
+    return 0;
+}
+
+int setupPIN(int pinNr, char *mode)
+{
+    char setupPin[50];
+    sprintf(setupPin, "echo %s > /sys/class/gpio/gpio%d/direction", mode, pinNr);
+    system(setupPin);
+    sleep(1);
     
-    pinMode(led,OUTPUT);
-    delay(10); 
+    return 0;
+}
+
+int writePIN(int pinNr, int value)
+{
+    char writePin[50];
+    sprintf(writePin, "echo %d > /sys/class/gpio/gpio%d/value", value, pinNr);
+    system(writePin);
+    sleep(0.1);
     
-    while(1)
-    {
-	    blink(led);
-	}
+    return 0;
+}
+
+int readPIN(int pinNr)
+{
+    int value;
+    char readPin[33];
+    sprintf(readPin, "cat /sys/class/gpio/gpio%d/value", pinNr);
+    // Do the magic tricks
+    sleep(0.1);
+    
+    return value;
+}
+
+int deinitPIN(int pinNr)
+{
+    char deinitPin[35];
+    sprintf(deinitPin, "echo %d > /sys/class/gpio/unexport", pinNr);
+    system(deinitPin);
+    sleep(0.1);
     
     return 0;
 }
