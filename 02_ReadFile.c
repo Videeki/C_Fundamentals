dac7fd00e45f74e9c0e5a09fbd294559c25aabc4
Videeki <videeki@gmail.com>
2023-08-06 11:43:47 +0200

    Imporve with arguments handling

--- 02_ReadFile.c
+++ 02_ReadFile.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(int argc, char* argv[])
+{
     FILE *fp;
     char *str;
     long size;
 
-    fp = fopen("AidStuffs//02_TestText.txt", "rb"); // read mode
+    fp = fopen(argv[1], "rb"); // read mode
 
     if (fp == NULL) {
         perror("Error while opening the file.\n");
