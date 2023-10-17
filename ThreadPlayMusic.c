f5db0c7d5c8a31b8bca2b278530c3edd71d2442e
Videeki <videeki@gmail.com>
2023-10-17 22:27:43 +0200

    Now it can play music as I want, yeah

--- ThreadPlayMusic.c
+++ ThreadPlayMusic.c
@@ -1,118 +1,110 @@
 // Compiling gcc -O2 -o ThreadPlayMusic ThreadPlayMusic.c -lmpg123 -lao -lpthread
 
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>  //Header file for sleep(). man 3 sleep for details.
 #include <pthread.h>
-#include <ao/ao.h>
-#include <mpg123.h>
-#include <wiringPi.h>
 
-#define BITS 8
-  
+#include "PlayMusic.h"
+#include "Queue.h"
+
+
+string_Queue_t playlist;
+int run = 1;
 // A normal C function that is executed as a thread 
 // when its name is specified in pthread_create()
-void* playMusic(void *path);
-const buttons[4] = {2,14,17,23};
-//enum 
-void* gpiohandling(void *targs);
+void* musicPlayer();
+
+
    
 int main()
 {
     pthread_t thread_id;
     int i = 0;
-
-    char path[] = "/media/videeki/Adatok/Zene/Vegyes/Amánjó főiskola he.mp3";
+    char path[255];
+    obtain(&playlist, "MusicQueue", 100);
 
     printf("Before Thread\n");
-    pthread_create(&thread_id, NULL, playMusic, (void*)&path);
-    for(i = 0; i < 100; i++)
+    pthread_create(&thread_id, NULL, musicPlayer, NULL);
+    int cmd;
+    while(run)
     {
-        printf("Printed main %d times\n", i);
-        sleep(1);
+        puts("\n");
+        printf("1.Add music\n");
+        printf("2.List playlist\n");
+        printf("3.Nr of music\n");
+        printf("4.Exit\n");
+        printf("Enter your choice of operations : ");
+        scanf("%d", &cmd);
+        
+        switch (cmd)
+        {
+            case 1:
+            {
+                char temp[255];
+                scanf("%s", temp);
+                enqueue(&playlist, temp);
+                break;
+            }
+            
+            case 2:
+            {
+                show(&playlist);
+                break;
+            }
+
+            case 3:
+            {
+                printf("Nr of Elements: %d\n", nrOfElements(&playlist));
+                break;
+            }
+
+            case 4:
+            {
+                run = 0;
+                break;
+            }
+
+            case 5:
+            {
+                exit(0);
+            }
+            
+            default:
+            printf("Incorrect choice \n");
+        }
     }
     pthread_join(thread_id, NULL);
     printf("After Thread\n");
-    exit(0);
-}
-
-void* playMusic(void *path)
-{
-    mpg123_handle *mh;
-    unsigned char *buffer;
-    size_t buffer_size;
-    size_t done;
-    int err;
-
-    int driver;
-    ao_device *dev;
-
-    ao_sample_format format;
-    int channels, encoding;
-    long rate;
-
-    /* initializations */
-    ao_initialize();
-    driver = ao_default_driver_id();
-    mpg123_init();
-    mh = mpg123_new(NULL, &err);
-    buffer_size = mpg123_outblock(mh);
-    buffer = (unsigned char*) malloc(buffer_size * sizeof(unsigned char));
-
-    /* open the file and get the decoding format */
-    mpg123_open(mh, (char*)path);
-    mpg123_getformat(mh, &rate, &channels, &encoding);
-
-    /* set the output format and open the output device */
-    format.bits = mpg123_encsize(encoding) * BITS;
-    format.rate = rate;
-    format.channels = channels;
-    format.byte_format = AO_FMT_NATIVE;
-    format.matrix = 0;
-    dev = ao_open_live(driver, &format, NULL);
-
-    /* decode and play */
-    while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK)
-        ao_play(dev, buffer, done);
-
-    /* clean up */
-    free(buffer);
-    ao_close(dev);
-    mpg123_close(mh);
-    mpg123_delete(mh);
-    mpg123_exit();
-    ao_shutdown();
-
+    flush(&playlist);
+    
     return 0;
 }
 
-void* gpiohandling(void *state)
+
+void* musicPlayer()
 {
-    switch ((int)state)
+    char path[255];
+
+    //puts("Music initialization");
+    initMusic();
+    
+    while(run)
     {
-        case 0:
+        if(nrOfElements(&playlist) == 0)
         {
-            wiringPiSetupGpio();
-            for(int i = 0; i < sizeof(buttons); i++)
-            {
-                pinMode(buttons[i],INPUT);
-                delay(10);
-            }
-            break;  
+            sleep(1);
         }
-        case 1:
-        {
-            for(int i = 0; i < sizeof(buttons); i++)
-            {
-                digitalRead(buttons[i]);
-                delay(10);
-            } 
-            break;  
+        else
+        {   
+            playMusic(dequeue(&playlist));
         }
-        default:
-        {
-            run = FALSE;
-            break;
-        }
+        
     }
+    
+    //puts("Music closed");
+    closeMusic();
+
 }
